300e1b1b8f4d17e3f63bc7ba9a6de59d6998d830
goldliondevX <eityeily@gmail.com>
2024-09-30 20:52:41 +0900

    update command line and add env

---
+++ src/config/index.ts
@@ -0,0 +1,28 @@
+import env from "dotenv";
+
+class Env {
+  constructor() {
+    env.config();
+  }
+
+  get(key: string) {
+    return env.parse(key);
+  }
+}
+
+env.config();
+
+const FROM_CONTENT_SPACE_ID = process.env.FROM_CONTENT_SPACE_ID ?? "";
+const FROM_CONTENT_DELIVERY_KEY = process.env.FROM_CONTENT_DELIVERY_KEY ?? "";
+
+const TO_CONTENT_SPACE_ID = process.env.TO_CONTENT_SPACE_ID ?? "";
+const TO_CONTENT_MANAGE_KEY = process.env.TO_CONTENT_MANAGE_KEY ?? "";
+
+export {
+  FROM_CONTENT_SPACE_ID,
+  FROM_CONTENT_DELIVERY_KEY,
+  TO_CONTENT_SPACE_ID,
+  TO_CONTENT_MANAGE_KEY,
+};
+
+export default Env;
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,24 @@
 import * as contentfulManager from "contentful-management";
 import yargs from "yargs";
-import * as chalk from "chalk";
 import contentfulClient from "contentful";
 import { cloneModel } from "./service/model";
 import { cloneContent } from "./service/content";
 import { cloneAssets } from "./service/assets";
+import { isEmpty } from "lodash";
+
+import {
+  TO_CONTENT_SPACE_ID,
+  TO_CONTENT_MANAGE_KEY,
+  FROM_CONTENT_SPACE_ID,
+  FROM_CONTENT_DELIVERY_KEY,
+} from "./config";
+
 interface Args {
-  "from-space-id": string;
-  "from-space-api-key": string;
-  "to-space-id": string;
-  "to-space-api-key": string;
   "with-content"?: boolean;
 }
 
 // Parse command line arguments
 const argv = yargs
-  .option("from-space-id", {
-    type: "string",
-    demandOption: true,
-    describe: "ID of the source space",
-  })
-  .option("from-space-api-key", {
-    type: "string",
-    demandOption: true,
-    describe: "API key for the source space",
-  })
-  .option("to-space-id", {
-    type: "string",
-    demandOption: true,
-    describe: "ID of the target space",
-  })
-  .option("to-space-api-key", {
-    type: "string",
-    demandOption: true,
-    describe: "API key for the target space",
-  })
   .option("with-content", {
     type: "boolean",
     describe: "Whether to copy content as well",
@@ -44,22 +28,24 @@ const argv = yargs
 const cloneSpace = async () => {
   try {
     const toClient = contentfulManager.createClient({
-      accessToken: argv["to-space-api-key"],
+      accessToken: TO_CONTENT_MANAGE_KEY,
     });
 
     const fromClient = contentfulClient.createClient({
-      space: argv["from-space-id"],
-      accessToken: argv["from-space-api-key"],
+      space: FROM_CONTENT_SPACE_ID,
+      accessToken: FROM_CONTENT_DELIVERY_KEY,
     });
 
     const toSpace = await (
-      await toClient.getSpace(argv["to-space-id"])
+      await toClient.getSpace(TO_CONTENT_SPACE_ID)
     ).getEnvironment("master");
 
     console.log("Successfully authenticated both spaces!");
     await cloneModel(fromClient, toSpace);
-    await cloneAssets(fromClient, toSpace);
-    await cloneContent(fromClient, toSpace);
+    if (argv["with-content"]) {
+      await cloneAssets(fromClient, toSpace);
+      await cloneContent(fromClient, toSpace);
+    }
   } catch (error: any) {
     console.error("Error during cloning process: ", error.message);
     if (error.name === "RateLimitError") {
@@ -70,8 +56,11 @@ const cloneSpace = async () => {
 
 // Validate inputs
 const validateInput = () => {
-  if (!argv["from-space-api-key"] || !argv["to-space-api-key"]) {
-    throw new Error("Invalid API key format.");
+  if (isEmpty(FROM_CONTENT_SPACE_ID) && isEmpty(TO_CONTENT_SPACE_ID)) {
+    throw new Error("Invalid SPACE ID.");
+  }
+  if (isEmpty(FROM_CONTENT_DELIVERY_KEY) && isEmpty(TO_CONTENT_MANAGE_KEY)) {
+    throw new Error("Invalid API key.");
   }
   // Additional validation logic for space names, etc.
 };
